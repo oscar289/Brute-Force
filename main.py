23ae4d5ecf7bb652126b1a55f13542c4f8426312
Oscar Jacquemin <oscar.jacquemin@gmail.com>
2022-12-31 11:36:56 +0100

    the skeleton of a function brute force

---
+++ main.py
@@ -0,0 +1,15 @@
+def brute_force(target, possibilities):
+    """
+    Attempt to find the target value by trying every possibility in the list of possibilities.
+
+    Parameters:
+    - target (any): The value that we are trying to find.
+    - possibilities (list): A list of potential values that could be the target.
+
+    Returns:
+    - The target value if it is found in the list of possibilities, or None if it is not found.
+    """
+    # write the body here
+
+
+    return None
